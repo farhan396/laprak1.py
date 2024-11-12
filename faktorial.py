0088232d4d981c81f5b68eff9019b54e3aed615f
farhan396 <made37247@gmail.com>
2024-11-12 06:52:25 -0800

    Create faktorial.py

---
+++ faktorial.py
@@ -0,0 +1,10 @@
+def faktorial (n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*(faktorial(n-1))
+    
+# Mencari nilai faktorial!
+a = int (input("masukkan nilai yang akan dicari: "))
+cari_faktorial = faktorial(a) 
+print ("nilai dari ", a,"! adalah ", cari_faktorial)
